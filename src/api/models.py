aadd554872d1b3cd03513417169344bae3aa932d
lucciii33 <angelomaiele@gmail.com>
2022-11-07 15:56:59 +0000

    more work

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, Table
+from sqlalchemy.orm import  relationship
+
 
 db = SQLAlchemy()
 
@@ -19,10 +22,12 @@ class User(db.Model):
         }
         
 class Restaurant(db.Model):
+    __tablename__ = "restaurant_table"
     id = db.Column(db.Integer, primary_key=True)
     rest_name = db.Column(db.String(120), unique=True, nullable=False)
     rest_address = db.Column(db.String(80), unique=False, nullable=False)
     rest_image = db.Column(db.String(300), unique=False, nullable=False)
+    review_table = relationship("Review")
     
     def __repr__(self):
         return f'<Restaurant {self.rest_name}>'
@@ -36,3 +41,26 @@ class Restaurant(db.Model):
             
             # do not serialize the password, its a security breach
         } 
+        
+class Review(db.Model):
+    __tablename__ = "review_table"
+    id = db.Column(db.Integer, primary_key=True)
+    rest_name = db.Column(db.String(120), unique=True, nullable=False)
+    review = db.Column(db.String(300), unique=False, nullable=False)
+    rating = db.Column(db.String(300), unique=False, nullable=False)
+    rest_id = Column(Integer, ForeignKey("restaurant_table.id"))
+    
+    def __repr__(self):
+        return f'<Restaurant {self.rest_name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "rest_name": self.rest_name,
+            "review": self.review,
+            "rating": self.rating,
+            "rest_id": self.rest_id,
+            
+            # do not serialize the password, its a security breach
+        } 
+        
--- src/api/routes.py
+++ src/api/routes.py
@@ -42,3 +42,53 @@ def get_rests():
     all_rests = list(map(lambda x: x.serialize(), rest_query))
     
     return jsonify(all_rests), 200
+
+
+@api.route('/rest/review', methods=['POST'])
+def post_review():
+    body = request.get_json()
+    rest_name = body['rest_name']
+    review = body['review']
+    rating = body['rating']
+    review_exist = Review.query.filter_by(rest_name=rest_name).first()
+    if rest_exist is not None:
+        raise APIException("rest already exist", 400)
+    review = Review(rest_name=rest_name, review=review, rating=rating,  )
+    db.session.add(review)
+    db.session.commit()
+
+    
+
+    return jsonify(review.serialize()), 200
+
+
+@api.route('/rest/review', methods=['GET'])
+def get_review():
+    
+    review_query = Review.query.all()
+    all_review = list(map(lambda x: x.serialize(), review_query))
+    
+    return jsonify(all_review), 200
+
+@api.route('/rest/review/<int:id>', methods=['GET'])
+def get_review_id(id):
+    # id = get_jwt_identity()
+    review_query = Review.query.filter_by(id)
+
+    
+    return jsonify(review_query.serialize()), 200
+
+@api.route('/rest/review/<int:id>', methods=['POST'])
+def post_review_id():
+    body = request.json
+
+    review_id = Review(rest_name=body['rest_name'], review=body['review'],rating=body['rating'])
+
+    db.session.add(review_id)
+    db.session.commit()
+
+    review = Review.query.filter_by(rest_id = id)
+    all_reviews= list(map(lambda x: x.serialize(), review))
+    
+
+    return jsonify(all_reviews), 200
